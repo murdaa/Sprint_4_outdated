2664707dce9d713133952b4a8a71c0574ddb83c7
Rita Sitnova <marg.sitnova@gmail.com>
2022-08-05 14:45:51 +0300

    Classes for Account and AccountTest added

---
+++ src/main/java/com/example/Praktikum.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Account account = new Account("Ольга Владимировна ");
+        account.checkNameToEmboss();
+    }
+
+}
